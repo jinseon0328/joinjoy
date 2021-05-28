08e4bf284f4c424f2cedabb0de28115cf3c815ef
jinseon0328 <wishofsun@daum.net>
2021-05-28 20:00:54 +0900

    2021-05-28

---
+++ app/src/main/java/com/osk/MailTest.java
@@ -0,0 +1,77 @@
+package com.osk;
+
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+// https://www.journaldev.com/2532/javamail-example-send-mail-in-java-smtp
+
+// https://offbyone.tistory.com/367
+public class MailTest {
+
+  public static void main(String[] args) {
+    System.out.println("SimpleEmail Start");
+
+    Properties props = System.getProperties();
+    props.put("mail.smtp.host", "smtp.naver.com");
+    props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+    props.put("mail.smtp.socketFactory.class",
+        "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+    props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+    props.put("mail.smtp.port", "465"); //SMTP Port
+
+
+    Session session = Session.getDefaultInstance(props, new Authenticator() {
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication("cc-_-@naver.com", "qlxmzoavm123");
+      }
+    });
+    System.out.println("Session created");
+
+    sendEmail(session, "desire_yo@naver.com","SimpleEmail Testing Subject", "SimpleEmail Testing Body");
+  }
+  /**
+   * Utility method to send simple HTML email
+   * @param session
+   * @param toEmail
+   * @param subject
+   * @param body
+   */
+  public static void sendEmail(Session session, String toEmail, String subject, String body){
+    try
+    {
+      MimeMessage msg = new MimeMessage(session);
+      //set message headers
+      msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+      msg.addHeader("format", "flowed");
+      msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+      msg.setFrom(new InternetAddress("cc-_-@naver.com", "NoReply-JD"));
+
+      //msg.setReplyTo(InternetAddress.parse("cc-_-@naver.com", false));
+
+      msg.setSubject(subject, "UTF-8");
+
+      msg.setText(body, "UTF-8");
+
+      msg.setSentDate(new Date());
+
+      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+      System.out.println("Message is ready");
+      Transport.send(msg);  
+
+      System.out.println("EMail Sent Successfully!!");
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
--- app/src/main/java/com/osk/team/web/QnaSendMailHandler.java
+++ app/src/main/java/com/osk/team/web/QnaSendMailHandler.java
@@ -1,148 +1,86 @@
 package com.osk.team.web;
 
+import java.io.IOException;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import com.osk.team.service.MemberService;
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Transport;
 
 
 @SuppressWarnings("serial")
 @WebServlet("/mail")
 public class QnaSendMailHandler extends HttpServlet {
 
+  // 1. 계정 정보를 입력하고 encoding 설정을 합니다.
+  private final String user = "cc-_-@naver.com";   // Sender Email
+  private final String password = "qlxmzoavm123";       // Sender Password
+  private final String ENC_TYPE = "EUC-KR";           // Content Encoding
+  private Properties prop;    // SMTP Setting information
+
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-    // 메일 관련 정보
-    String host = "smtp.naver.com";
-    final String username = "desire_yo";
-    final String password = "bitcamp1111";
-    int port=465;
+    this.prop = getProperties();
+  }
 
-    // 메일 내용
-    String recipient = "wishofsun@daum.net";
-    String subject = "네이버를 사용한 발송 테스트입니다.";
-    String body = "내용 무";
+  // 2. 아까 확인했던 SMTP 정보를 기입합니다.
+  private Properties getProperties() {
 
-    MemberService qnaService = (MemberService) request.getServletContext().getAttribute("qnaService");
+    Properties prop = new Properties();
+    prop.put("mail.smtp.host", "smtp.naver.com");
+    prop.put("mail.smtp.port", 465);
+    prop.put("mail.smtp.auth", "true");
+    return prop;
 
+  }
 
-    // 먼저 아이디로 회원정보를 받아오고 가져온 데이터에서 email값을 비교하여 존재하지 않으면 인증메일 보내지 못함
-    /*
-     * Member m = new MemberService().memberLogin(memberId); if(m==null ||
-     * !m.getEmail().equals(email)) { req.setAttribute("msg",
-     * "아이디나 이메일 정보가 맞지 않습니다"); req.setAttribute("loc", "/member/searchPw");
-     * req.getRequestDispatcher("/views/common/msg.jsp").forward(req, resp); return;
-     * }
-     */
-    // mail server 설정
-    String host = "smtp.naver.com";
-    String user = "desire_yo@naver.com"; // 자신의 네이버 계정
-    String password = "";// 자신의 네이버 패스워드
+  // 3. SMTP정보를 기반으로 인증을 받아 Session을 생성합니다.
+  private Session getSession() {
+    return  Session.getDefaultInstance(prop, new Authenticator() {
 
-    // 메일 받을 주소
-    String to_email = m.getEmail();
-    String to_email = "dhsdb1541@naver.com";
-
-    // SMTP 서버 정보를 설정한다.
-    Properties props = new Properties();
-    props.put("mail.smtp.host", host);
-    props.put("mail.smtp.port", 465);
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.ssl.enable", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
-
-    // 인증 번호 생성기
-    StringBuffer temp = new StringBuffer();
-    Random rnd = new Random();
-    for (int i = 0; i < 10; i++) {
-      int rIndex = rnd.nextInt(3);
-      switch (rIndex) {
-        case 0:
-          // a-z
-          temp.append((char) ((rnd.nextInt(26)) + 97));
-          break;
-        case 1:
-          // A-Z
-          temp.append((char) ((rnd.nextInt(26)) + 65));
-          break;
-        case 2:
-          // 0-9
-          temp.append((rnd.nextInt(10)));
-          break;
-      }
-    }
-    String AuthenticationKey = temp.toString();
-    System.out.println(AuthenticationKey);
-
-    Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
       @Override
       protected PasswordAuthentication getPasswordAuthentication() {
         return new PasswordAuthentication(user, password);
       }
     });
+  }
+  // 4. Mail을 전송하는 부분입니다.
+  public void send() throws ServletException {
 
-    // email 전송
     try {
-      MimeMessage msg = new MimeMessage(session);
-      msg.setFrom(new InternetAddress(user, "KH"));
-      msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
-
-      // 메일 제목
-      msg.setSubject("안녕하세요  인증 메일입니다.");
-      // 메일 내용
-      msg.setText("인증 번호는 :" + temp);
-
-      Transport.send(msg);
-      System.out.println("이메일 전송");
+      Session session = getSession();
+      MimeMessage message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(user));
+      // Receiver mail
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress("desire_yo@naver.com"));
+      // Suject
+      message.setSubject("test입니다");
+      // Text
+      MimeBodyPart content = new MimeBodyPart();
+      content.setText("test내용입니다.");
+      // Write contents(Text & File)
+      Multipart mp = new MimeMultipart();
+      mp.addBodyPart(content);
+      message.setContent(mp);
+      // Send the message
+      Transport.send(message);
+      System.out.println("전송 완료!");
 
     } catch (Exception e) {
-      e.printStackTrace();// TODO: handle exception
+      throw new ServletException("전송 실패!");
     }
-    HttpSession saveKey = request.getSession();
-    saveKey.setAttribute("AuthenticationKey", AuthenticationKey);
-    // 패스워드 바꿀때 뭘 바꿀지 조건에 들어가는 id
-    /*
-     * req.setAttribute("id", memberId);
-     * req.getRequestDispatcher("/views/login_myPage/searchPasswordEnd.jsp").forward
-     * (req, resp);
-     */
   }
-  Properties props = System.getProperties();
-
-
-  props.put("mail.smtp.host", host);
-  props.put("mail.smtp.port", port);
-  props.put("mail.smtp.auth", "true");
-  props.put("mail.smtp.ssl.enable", "true");
-  props.put("mail.smtp.ssl.trust", host);
-
-  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-    String un=username;
-    String pw=password;
-    @Override
-    protected PasswordAuthentication getPasswordAuthentication() {
-      return new PasswordAuthentication(un, pw);
-    }
-  });
-  session.setDebug(true); //for debug
-
-  Message mimeMessage = new MimeMessage(session);
-  mimeMessage.setFrom(new InternetAddress("smtp.naver.com"));
-  mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-  mimeMessage.setSubject(subject);
-  mimeMessage.setText(body);
-  Transport.send(mimeMessage);
-}
 }
