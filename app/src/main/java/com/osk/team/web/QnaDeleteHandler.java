47c6aef84628f95fa11d467f2f52e0bc4c732426
jinseon0328 <wishofsun@daum.net>
2021-05-30 19:23:22 +0900

    2021-05-30 before

--- app/src/main/java/com/osk/team/web/QnaDeleteHandler.java
+++ app/src/main/java/com/osk/team/web/QnaDeleteHandler.java
@@ -1,14 +1,11 @@
 package com.osk.team.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.osk.team.domain.Member;
 import com.osk.team.domain.Qna;
 import com.osk.team.service.QnaService;
@@ -22,14 +19,6 @@ public class QnaDeleteHandler extends HttpServlet {
       throws ServletException, IOException {
 
     QnaService qnaService = (QnaService) request.getServletContext().getAttribute("qnaService");
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>QnA 삭제</title>");
 
     try {
       int no = Integer.parseInt(request.getParameter("no"));
@@ -46,26 +35,10 @@ public class QnaDeleteHandler extends HttpServlet {
 
       qnaService.delete(no);
 
-      out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>QnA 삭제</h1>");
-      out.println("<p>QnA를 삭제하였습니다.</p>");
+      response.sendRedirect("list");
 
     } catch (Exception e) {
-      StringWriter strWriter = new StringWriter();
-      PrintWriter printWriter = new PrintWriter(strWriter);
-      e.printStackTrace(printWriter);
-
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>QnA 삭제 오류</h1>");
-      out.printf("<p>%s</p>\n", e.getMessage());
-      out.printf("<pre>%s</pre>\n", strWriter.toString());
-      out.println("<p><a href='list'>목록</a></p>");
+      throw new ServletException(e);
     }
-
-    out.println("</body>");
-    out.println("</html>");
   }
 }
--- app/src/main/java/com/osk/team/web/QnaUpdateHandler.java
+++ app/src/main/java/com/osk/team/web/QnaUpdateHandler.java
@@ -1,7 +1,5 @@
 package com.osk.team.web;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,16 +15,10 @@ public class QnaUpdateHandler extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+
     QnaService qnaService = (QnaService) request.getServletContext().getAttribute("qnaService");
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>QnA 변경</title>");
     try {
-      request.setCharacterEncoding("UTF-8");
       int no = Integer.parseInt(request.getParameter("no"));
       Qna oldQna = qnaService.get(no);
       if (oldQna == null) {
@@ -44,26 +36,10 @@ public class QnaUpdateHandler extends HttpServlet {
       qna.setAnswer(request.getParameter("answer"));
       qnaService.update(qna);
 
-      out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>QnA 변경</h1>");
-      out.println("<p>QnA을 변경하였습니다.</p>");
-
+      response.sendRedirect("list");
 
     } catch (Exception e) {
-      StringWriter strWriter = new StringWriter();
-      PrintWriter printWriter = new PrintWriter(strWriter);
-      e.printStackTrace(printWriter);
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>QnA 변경 오류</h1>");
-      out.printf("<p>%s</p>\n", e.getMessage());
-      out.printf("<pre>%s</pre>\n", strWriter.toString());
-      out.println("<p><a href='list'>목록</a></p>");
+      throw new ServletException(e);
     }
-
-    out.println("</body>");
-    out.println("</html>");
   }
 }
