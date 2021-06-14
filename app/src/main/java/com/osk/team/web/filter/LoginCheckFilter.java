bd30caa07d051bdd7fe5ae84521e87060905be88
jinseon0328 <wishofsun@daum.net>
2021-06-14 23:59:48 +0900

    2021-06-14

--- app/src/main/java/com/osk/team/web/filter/LoginCheckFilter.java
+++ app/src/main/java/com/osk/team/web/filter/LoginCheckFilter.java
@@ -8,9 +8,12 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @WebFilter("/*")
 public class LoginCheckFilter implements Filter {
+  static Logger logger = LogManager.getLogger();
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain nextChain)
@@ -18,17 +21,21 @@ public class LoginCheckFilter implements Filter {
 
     HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-    String servletPath = httpRequest.getServletPath();
+    logger.debug("{}{}", httpRequest.getServletPath(), httpRequest.getPathInfo());
 
-    if (servletPath.startsWith("/app")) {
-
+    if (httpRequest.getServletPath().startsWith("/app")) {
       String pathInfo = httpRequest.getPathInfo();
-      if (pathInfo.endsWith("add") ||
+      if (pathInfo.endsWith("/member/add")) {
+        System.out.println("회원가입 경로");
+      } else if (pathInfo.endsWith("add") ||
           pathInfo.endsWith("update") ||
           pathInfo.endsWith("detail") ||
           pathInfo.endsWith("delete")) {
 
+        logger.debug("로그인 검사!");
+
         if (httpRequest.getSession().getAttribute("loginUser") == null) {
+          logger.debug("로그인을 하지 않았음!");
           response.setContentType("text/plain;charset=UTF-8");
           response.getWriter().println("로그인 해야 합니다!");
           return;
